28f6a65d936e51ff62d9e5bd0a83e55e7088bb58
maxm24 <709003@student.dusd.net>
2023-02-15 15:21:21 -0800

    finished

--- PlayerControlMiramontesP7/Assets/Scripts/PlayerController.cs
+++ PlayerControlMiramontesP7/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +18,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //We'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //We'll move the vehicle forward vertically on a forward input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Allows us to move the vehicle on a horizontal input, emaning we can turn it.
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
